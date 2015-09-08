0f9f5dfec0736ea262df67c6b82335f476798d13
LawrenceHan <yangfei6565@163.com>
2015-09-08 21:07:59 +0800

    Created project

---
+++ LudoBottle/LudoBottle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LudoBottle
+//
+//  Created by Hanguang on 9/8/15.
+//  Copyright (c) 2015 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
